3783f8ea9afc145b0bbc137887129e364ad185a1
David Clausen <davidclausen2051@gmail.com>
2024-07-01 21:48:29 -0500

    enable all examples

--- src/index.ts
+++ src/index.ts
@@ -6,34 +6,30 @@ import { runHalfAdderExample } from '../examples/half-adder';
 import { runIrisExample } from '../examples/iris';
 import { runLargestExample } from '../examples/largest';
 
-// console.log("Running XOR Example:");
-// runXORExample();
+console.log("Running XOR Example:");
+runXORExample();
 
-// console.log("\n----------------------------\n");
+console.log("\n----------------------------\n");
+console.log("Running AND Example:");
+runANDExample();
 
-// console.log("Running AND Example:");
-// runANDExample();
+console.log("\n----------------------------\n");
+console.log("Running OR Example:");
+runORExample();
 
-// console.log("\n----------------------------\n");
+console.log("\n----------------------------\n");
+console.log("Running NAND Example:");
+runNANDExample();
 
-// console.log("Running OR Example:");
-// runORExample();
-
-// console.log("\n----------------------------\n");
-
-// console.log("Running NAND Example:");
-// runNANDExample();
-
-// console.log("\n----------------------------\n");
-
-// console.log("Running Half Adder Example:");
-// runHalfAdderExample();
+console.log("\n----------------------------\n");
+console.log("Running Half Adder Example:");
+runHalfAdderExample();
 
 console.log("\n----------------------------\n");
 console.log("Running Largest Example:");
 runLargestExample();
 
-// console.log("\n----------------------------\n");
-// console.log("Running Iris Classification Example:");
-// runIrisExample();
+console.log("\n----------------------------\n");
+console.log("Running Iris Classification Example:");
+runIrisExample();
 
